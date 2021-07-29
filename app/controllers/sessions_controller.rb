9c78f2386612553203dd89e7b81e08dfbf2be312
Hanafi kopihub <hanafi@kopihub.com>
2021-07-29 22:19:43 +0700

    fix error when get token login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,11 +4,20 @@ class SessionsController < Devise::SessionsController
   private
 
   def respond_with(resource, _opts = {})
-    render_jsonapi_response(resource)
+    # render_jsonapi_response(resource)
+    @data = {
+      token: current_token,
+      email: resource.email
+    }
+    render json: { :data => @data, :status => 200 }
+
   end
 
   def respond_to_on_destroy
     head :no_content
   end
 
+  def current_token
+    request.env['warden-jwt_auth.token']
+  end
 end
